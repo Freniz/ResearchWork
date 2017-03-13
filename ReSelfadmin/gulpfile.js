7bd833805e20e0bb324cee3f36e7482d538f9f11
BenazirKhan <benazir@freniz.com>
2017-03-13 19:27:43 +0530

    PD-36 menu heading alignment changes

--- ReSelfadmin/gulpfile.js
+++ ReSelfadmin/gulpfile.js
@@ -18,14 +18,14 @@ gulp.task('js', function(){
 });
 
 gulp.task('cssmin', function(){
-   gulp.src(['bootstrap/css/bootstrap.css','build/fontawesome/css/font-awesome.css','dist/css/AdminLTE.css','bootstrap/css/bootstrap.css','dist/css/skins/_all-skins.css'])
+   gulp.src(['bootstrap/css/bootstrap.css','build/fontawesome/css/font-awesome.css','dist/css/AdminLTE.css','bootstrap/css/bootstrap.css','dist/css/skins/_all-skins.css','css/style.css'])
    .pipe(concat('freniz.styles.min.css'))
    .pipe(minify())
    .pipe(gulp.dest('css/'));
 });
 
 gulp.task('css', function(){
-   gulp.src(['bootstrap/css/bootstrap.css','build/fontawesome/css/font-awesome.css','dist/css/AdminLTE.css','dist/css/skins/_all-skins.css'])
+   gulp.src(['bootstrap/css/bootstrap.css','build/fontawesome/css/font-awesome.css','dist/css/AdminLTE.css','dist/css/skins/_all-skins.css','css/style.css'])
    .pipe(concat('freniz.styles.css'))
    .pipe(gulp.dest('css/'));
 });
